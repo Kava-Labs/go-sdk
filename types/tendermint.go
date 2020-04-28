bbbcbc49159245604128cfb167379929cae6e56e
denalimarsh <denalimarsh@gmail.com>
2020-04-28 13:48:14 -0700

    add wrappers for tmhash, log

--- types/tendermint.go
+++ types/tendermint.go
@@ -1,8 +1,18 @@
 package types
 
 import (
+	"github.com/tendermint/tendermint/crypto/tmhash"
 	tmbytes "github.com/tendermint/tendermint/libs/bytes"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 // HexBytes is a wrapper around tmbytes.HexBytes
 type HexBytes tmbytes.HexBytes
+
+// Sum is a wrapper around tmhash.Sum
+func Sum(data []byte) []byte {
+	return tmhash.Sum(data)
+}
+
+// Logger is a wrapper around log.Logger
+type Logger log.Logger
