a6f39ae493984a686a95244a834586ae226ec634
denalimarsh <denalimarsh@gmail.com>
2020-04-29 21:17:29 -0700

    tendermint logger, kava logger

--- types/tendermint.go
+++ types/tendermint.go
@@ -8,6 +8,27 @@ import (
 	// ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
+// --------------------------- Logger ---------------------------
+
+// TendermintLogger is an interface that enables loggers to extend KavaLogger
+type TendermintLogger interface {
+	Debug(msg string, keyvals ...interface{})
+	Info(msg string, keyvals ...interface{})
+	Error(msg string, keyvals ...interface{})
+}
+
+// KavaLogger implements method With, abstracting the tendermint dependency
+type KavaLogger struct {
+	TendermintLogger
+}
+
+// With adds a set of key-values to KavaLogger
+func (l *KavaLogger) With(keyvals ...interface{}) log.Logger {
+	return l
+}
+
+// --------------------------- HexBytes ---------------------------
+
 // HexBytes is a wrapper around tmbytes.HexBytes
 type HexBytes tmbytes.HexBytes
 
@@ -16,9 +37,7 @@ func Sum(data []byte) []byte {
 	return tmhash.Sum(data)
 }
 
-// Logger is a wrapper around log.Logger
-type Logger log.Logger
-
+// TODO: required?
 type PrivKey interface {
 	crypto.PrivKey
 }
