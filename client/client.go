6527320a09939a4ac989dbe99bd43cfc3a76a99c
Ruaridh <rhuairahrighairidh@users.noreply.github.com>
2020-09-28 15:47:08 +0100

    Migrate to imported kava types (#21)
    * remove kava types
    
    * import from non forked dependencies
    
    * import kava in go mod
    
    * update client for new tendermint version

--- client/client.go
+++ client/client.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	sdk "github.com/kava-labs/cosmos-sdk/types"
-	authtypes "github.com/kava-labs/cosmos-sdk/x/auth/types"
-	"github.com/kava-labs/tendermint/libs/log"
-	rpcclient "github.com/kava-labs/tendermint/rpc/client"
-	ctypes "github.com/kava-labs/tendermint/rpc/core/types"
-	tmtypes "github.com/kava-labs/tendermint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/tendermint/tendermint/libs/log"
+	rpcclient "github.com/tendermint/tendermint/rpc/client/http"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 	"github.com/tendermint/go-amino"
 
 	"github.com/kava-labs/go-sdk/keys"
@@ -25,7 +25,7 @@ type KavaClient struct {
 // NewKavaClient creates a new KavaClient
 func NewKavaClient(cdc *amino.Codec, mnemonic string, coinID uint32, rpcAddr string) *KavaClient {
 	// Set up HTTP client
-	http, err := rpcclient.NewHTTP(rpcAddr, "/websocket")
+	http, err := rpcclient.New(rpcAddr, "/websocket")
 	if err != nil {
 		panic(err)
 	}
